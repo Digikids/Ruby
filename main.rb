1119a6ac168144a3a12622b93b0f95cba89df7ef
Steve <muchendumaina66@gmail.com>
2021-05-17 06:16:48 +0300

    First Commit

---
+++ main.rb
@@ -0,0 +1,40 @@
+print "Hello World"
+puts "Stephen Maina"
+
+puts "There once was a man named Steve"
+puts "He was a programmer"
+puts "He really liked being a programmer"
+puts "but didn't like the name Steve."
+
+character_name = "Joshua"
+character_job = "Priest"
+
+
+puts ("There once was a man named " + character_name)
+puts ("He was a " + character_job)
+puts ("He really liked being a " + character_job)
+puts ("but didn't like the name "  + character_name)
+
+
+puts "Digikids\" Academy" 
+
+puts "Enter your name: "
+name = gets.chomp()
+puts ("Hello " + name + ", you are cool!")
+
+#calculator
+puts "Enter a number: "
+num1 = gets.chomp()#.to_f
+puts "Enter another number: "
+num2 = gets.chomp()#.to_f
+
+#puts (num1 + num2)
+#puts (num1.to_i + num2.to_i)
+puts (num1 + num2)
+
+
+def sayhi
+    puts "Hello User"
+end
+
+sayhi
