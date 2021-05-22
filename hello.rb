4f71f3980b1574c3f6dee988d6a5a223843fe599
Steve <muchendumaina66@gmail.com>
2021-05-22 12:41:58 +0300

    Updates

---
+++ hello.rb
@@ -0,0 +1,16 @@
+puts "Hello World"
+puts "Hello World"
+
+#Draw shape
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
+
+name = "Uhuru"
+occupation = "President"
+
+puts ("There once was a man called " + name)
+puts ("He was a " + occupation)
+puts ("He liked being a " + occupation)
+puts ("But did not like the name " + name)
